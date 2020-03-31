a9235cf8116a9ee39c2f4b4eba93c0c20519365f
Jean Nóbrega Nunes <42788439+NaejNunes@users.noreply.github.com>
2020-03-31 20:46:56 -0300

    projeto finalizado

---
+++ Super Country Race/Assets/Scripts/CheckPointController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointController : MonoBehaviour
+{
+    public GameController gameController;
+
+    private Transform[] checkpoints;
+
+    private int maxCheckpoints = 0;
+    private int checkpointAtual = 0;
+    private int voltaAtual = 0;
+    private int maximoVoltas;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        GameObject checkpointContainer = GameObject.FindGameObjectWithTag("CheckpointsContainer");
+
+        checkpoints = new Transform[checkpointContainer.transform.childCount];
+
+        for (int i = 0; i < checkpointContainer.transform.childCount; i++)
+        {
+            checkpoints[i] = checkpointContainer.transform.GetChild(i);
+        }
+    }
+    void Start()
+    {
+        maximoVoltas = gameController.maximoVoltas;
+        maxCheckpoints = checkpoints.Length - 1;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Checkpoint"))
+        {
+            if (other.transform == checkpoints[checkpointAtual])
+            {
+                if (checkpointAtual < maxCheckpoints)
+                {
+                    if (checkpointAtual == 0)
+                    {
+                        if (voltaAtual == maximoVoltas)
+                        {
+                            gameController.FinishRace(gameObject);
+                        }
+                        else
+                        {
+                            voltaAtual++;
+                            gameController.UpdateVoltas(voltaAtual);
+                        }
+                    }
+                    checkpointAtual++;
+                }
+                else {
+                    checkpointAtual = 0;
+                }
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Super Country Race/Assets/Scripts/GameController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] players;
+    public Text hudPrincipal, hudVoltas;
+    public int voltaAtual = 0, maximoVoltas = 5;
+
+    private float timer, delay = 1f;
+
+    private int contador = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = Time.time;
+
+        foreach (GameObject m_gameObject in players)
+        {
+            m_gameObject.GetComponent<PlayerController>().enabled = false;
+        }
+
+        hudVoltas.text = string.Format("{0}/{1}", voltaAtual, maximoVoltas);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (contador > 0)
+        {
+            if (delay < (Time.time - timer))
+            {
+                timer = Time.time;
+                contador--;
+            }
+
+            switch (contador)
+            {
+                case 4:
+                    hudPrincipal.text = "Super Country Race";
+                    break;
+                case 3:
+                    hudPrincipal.text = "READY!";
+                    break;
+                case 2:
+                    hudPrincipal.text = "SET!";
+                    break;
+                case 1:
+                    hudPrincipal.text = "GO!";
+                    foreach (GameObject m_gameObject in players)
+                    {
+                        m_gameObject.GetComponent<PlayerController>().enabled = true;
+                    }
+                    break;
+
+                case 0:
+                    hudPrincipal.text = "";
+                    break;
+
+                default:
+                    break;
+            }          
+        }
+    }
+
+    public void UpdateVoltas(int voltaPlayer)
+    {
+        if (voltaPlayer > voltaAtual)
+        {
+            voltaAtual = voltaPlayer;
+            hudVoltas.text = string.Format("{0}/{1}", voltaAtual, maximoVoltas);
+        }
+    }
+
+    public void FinishRace(GameObject winner)
+    {
+        foreach (GameObject m_gameObject in players)
+        {
+            if (m_gameObject == winner)
+            {
+                m_gameObject.GetComponent<PlayerController>().enabled = false;
+            }
+            else {
+                m_gameObject.SetActive(false);
+            }
+        }
+        hudPrincipal.text = string.Format("Player {0} ganhou!!!", winner.GetComponent<PlayerController>().playerID);
+    }
+}
